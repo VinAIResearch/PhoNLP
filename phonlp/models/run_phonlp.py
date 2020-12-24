973962feceb34f89df2c2cc7e1df5de93634833b
vinai <v.linhnt@vinai.io>
2020-12-24 15:42:15 +0700

    phonlp

--- phonlp/models/run_phonlp.py
+++ phonlp/models/run_phonlp.py
@@ -183,14 +183,13 @@ def train(args):
         {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
-    num_train_optimization_steps = int(args['num_epoch'] * len(train_batch_pos) * args['accumulation_steps'])
+    num_train_optimization_steps = int(args['num_epoch'] * len(train_batch_pos) / args['accumulation_steps'])
     optimizer = AdamW(optimizer_grouped_parameters, lr=args['lr'],
                       correct_bias=False)  # To reproduce BertAdam specific behavior set correct_bias=False
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=5,
                                                 num_training_steps=num_train_optimization_steps)
     get_constant_schedule(optimizer)
     for epoch in range(args['num_epoch']):
-
         ####
         optimizer.zero_grad()
         print(" EPOCH  : ", epoch)
